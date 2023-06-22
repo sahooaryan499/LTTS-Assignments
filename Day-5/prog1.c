8a207247d54c332358a1e543f46b318625033450
sahooaryan499 <sahooaryan499@gmail.com>
2023-06-22 15:28:14 +0530

    Array of Structures

--- Day-5/prog1.c
+++ Day-5/prog1.c
@@ -1,24 +1,35 @@
+/*Assume User will be providing input in the form of a string as show below. 
+Write a function to parse the string and initialize an array of structures. 
+
+Example String : "1001 Aron 100.00" 
+Example Structure : 
+    struct Student{
+        int rollno;
+        char name[20];
+        float marks;
+    };*/
+
 #include <stdio.h>
 #include <string.h>
 
 struct Student {
-    int rollno;
+    int roll;
     char name[20];
     float marks;
 };
 
-void parseInput(struct Student* studentArray, int arraySize) {
-    for (int i = 0; i < arraySize; i++) {
+void parseInput(struct Student* studArray, int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
         printf("Enter details for Student %d:\n", i + 1);
 
         printf("Roll No: ");
-        scanf("%d", &studentArray[i].rollno);
+        scanf("%d", &studArray[i].roll);
 
         printf("Name: ");
-        scanf("%s", studentArray[i].name);
+        scanf("%s", studArray[i].name);
 
         printf("Marks: ");
-        scanf("%f", &studentArray[i].marks);
+        scanf("%f", &studArray[i].marks);
 
         printf("\n");
     }
@@ -37,7 +48,7 @@ int main() {
     // Print the initialized structures
     for (int i = 0; i < numStudents; i++) {
         printf("Student %d:\n", i + 1);
-        printf("Roll No: %d\n", students[i].rollno);
+        printf("Roll No: %d\n", students[i].roll);
         printf("Name: %s\n", students[i].name);
         printf("Marks: %.2f\n", students[i].marks);
         printf("\n");
